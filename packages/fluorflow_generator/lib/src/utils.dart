892dba00b5b7d72a6a3174d9ab8781d3d608b526
Nando Schär <nando@smartive.ch>
2025-04-23 09:42:50 +0200

    chore: adapt utils.dart to new analyzer api

--- packages/fluorflow_generator/lib/src/utils.dart
+++ packages/fluorflow_generator/lib/src/utils.dart
@@ -5,8 +5,10 @@ import 'package:code_builder/code_builder.dart' as cb;
 import 'package:source_gen/source_gen.dart';
 
 TEnum getEnumFromAnnotation<TEnum extends Enum>(
-    List<TEnum> values, DartObject enumField,
-    [TEnum? defaultValue]) {
+  List<TEnum> values,
+  DartObject enumField, [
+  TEnum? defaultValue,
+]) {
   final index = enumField.getField('index')?.toIntValue();
   if (index == null && defaultValue != null) {
     return defaultValue;
@@ -23,60 +25,87 @@ cb.Reference recursiveTypeReference(
 
   return switch (t) {
     VoidType() || DynamicType() => cb.refer(t.getDisplayString()),
-    DartType(alias: InstantiatedTypeAliasElement(:final element)) =>
-      cb.refer(element.name, lib.pathToElement(element).toString()),
+    DartType(alias: InstantiatedTypeAliasElement(:final element2)) => cb.refer(
+      element2.displayName,
+      element2.library2.uri.toString(),
+    ),
     FunctionType(
       :final returnType,
-      :final parameters,
-      :final typeFormals,
-      :final nullabilitySuffix
+      :final formalParameters,
+      :final typeParameters,
+      :final nullabilitySuffix,
     ) =>
-      cb.FunctionType((b) => b
-        ..isNullable =
-            forceNullable || nullabilitySuffix == NullabilitySuffix.question
-        ..returnType = mapRef(returnType)
-        ..requiredParameters.addAll(parameters
-            .where((p) => p.isRequiredPositional)
-            .map((p) => p.type)
-            .map(mapRef))
-        ..optionalParameters.addAll(parameters
-            .where((p) => p.isOptionalPositional)
-            .map((p) => p.type)
-            .map(mapRef))
-        ..namedRequiredParameters.addAll({
-          for (final p in parameters.where((p) => p.isRequiredNamed))
-            p.name: mapRef(p.type)
-        })
-        ..namedParameters.addAll({
-          for (final p in parameters.where((p) => p.isOptionalNamed))
-            p.name: mapRef(p.type)
-        })
-        ..types.addAll(typeFormals
-            .where((tf) => tf.bound != null)
-            .map((tf) => tf.bound!)
-            .map(mapRef))),
+      cb.FunctionType(
+        (b) =>
+            b
+              ..isNullable =
+                  forceNullable ||
+                  nullabilitySuffix == NullabilitySuffix.question
+              ..returnType = mapRef(returnType)
+              ..requiredParameters.addAll(
+                formalParameters
+                    .where((p) => p.isRequiredPositional)
+                    .map((p) => p.type)
+                    .map(mapRef),
+              )
+              ..optionalParameters.addAll(
+                formalParameters
+                    .where((p) => p.isOptionalPositional)
+                    .map((p) => p.type)
+                    .map(mapRef),
+              )
+              ..namedRequiredParameters.addAll({
+                for (final p in formalParameters.where(
+                  (p) => p.isRequiredNamed,
+                ))
+                  p.displayName: mapRef(p.type),
+              })
+              ..namedParameters.addAll({
+                for (final p in formalParameters.where(
+                  (p) => p.isOptionalNamed,
+                ))
+                  p.displayName: mapRef(p.type),
+              })
+              ..types.addAll(
+                typeParameters
+                    .where((tf) => tf.bound != null)
+                    .map((tf) => tf.bound!)
+                    .map(mapRef),
+              ),
+      ),
     RecordType(
       :final positionalFields,
       :final namedFields,
-      :final nullabilitySuffix
+      :final nullabilitySuffix,
     ) =>
-      cb.RecordType((b) => b
-        ..isNullable =
-            forceNullable || nullabilitySuffix == NullabilitySuffix.question
-        ..positionalFieldTypes
-            .addAll(positionalFields.map((f) => mapRef(f.type)))
-        ..namedFieldTypes.addIterable(namedFields,
-            key: (f) => f.name, value: (f) => mapRef(f.type))),
-    _ => cb.TypeReference((b) => b
-      ..isNullable =
-          forceNullable || t.nullabilitySuffix == NullabilitySuffix.question
-      ..symbol = t.element?.name ?? t.getDisplayString()
-      ..types.addAll(switch (t) {
-        ParameterizedType(:final typeArguments) =>
-          typeArguments.map(mapRef).toList(),
-        _ => [],
-      })
-      ..url =
-          t.element == null ? null : lib.pathToElement(t.element!).toString()),
+      cb.RecordType(
+        (b) =>
+            b
+              ..isNullable =
+                  forceNullable ||
+                  nullabilitySuffix == NullabilitySuffix.question
+              ..positionalFieldTypes.addAll(
+                positionalFields.map((f) => mapRef(f.type)),
+              )
+              ..namedFieldTypes.addIterable(
+                namedFields,
+                key: (f) => f.name,
+                value: (f) => mapRef(f.type),
+              ),
+      ),
+    _ => cb.TypeReference(
+      (b) =>
+          b
+            ..isNullable =
+                forceNullable ||
+                t.nullabilitySuffix == NullabilitySuffix.question
+            ..symbol = t.element3?.displayName ?? t.getDisplayString()
+            ..types.addAll(switch (t) {
+              ParameterizedType(:final typeArguments) =>
+                typeArguments.map(mapRef).toList(),
+              _ => [],
+            })
+            ..url = t.element3?.library2?.uri.toString(),
+    ),
   };
 }
